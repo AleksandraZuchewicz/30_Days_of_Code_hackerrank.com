fd8357b47655b0dd305524d316bd7cfcb0781cec
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-11-16 09:48:46 +0000

    Day 4

---
+++ Day_4_Class_Instance.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(initialAge) {
+    if (initialAge < 0) {
+      this.age = 0;
+      console.log("Age is not valid, setting age to 0.");
+    } else {
+      this.age = initialAge;
+    }
+  }
+  yearPasses() {
+    this.age = this.age + 1;
+  }
+  amIOld() {
+    if (this.age < 13) {
+      console.log("You are young.");
+    } else if (this.age >= 13 && this.age < 18) {
+      console.log("You are a teenager.");
+    } else {
+      console.log("You are old.");
+    }
+  }
+}
